e9a71a63ad3d33124c5974685584092e571a3fa8
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-20 20:20:31 +0600

    type alis

--- moduleOne/src/alis.ts
+++ moduleOne/src/alis.ts
@@ -1,9 +1,23 @@
 // ts type alis
 
 type UserType = {
-    name: string;
-    age: number;
-    location: string;
-    profession: string
-}
+  name: string;
+  age?: number;
+  location: string;
+  profession: string;
+};
 
+const userOne: UserType = {
+  name: "Rezwan Rahim",
+  age: 23,
+  location: "Dhaka",
+  profession: "Jr Developer",
+};
+
+const userTwo: UserType = {
+  name: "Rezwan Rahim Rupak",
+  location: "Dhaka",
+  profession: "Jr Developer",
+};
+
+console.log(userOne);
