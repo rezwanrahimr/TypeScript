7c97780af26065b42e9fd55059649a4702d2b5db
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-21 19:35:20 +0600

    ts type assertions

---
+++ modules/src/Module 2/TypeAssertions.ts
@@ -0,0 +1,15 @@
+// Type assertions instruct the TypeScript compiler to treat a value as a specified type.
+
+function kgTogram(gram: string | number): string | number | void {
+  if (typeof gram === "number") {
+    const value = gram * 1000;
+    return value;
+  }
+  if (typeof gram === "string") {
+    const value = parseFloat(gram) * 1000;
+    return `The Result is : ${value}`;
+  }
+}
+
+const getKgTogramNumber = kgTogram(20) as number;
+const getKgTogramString = kgTogram("20") as string;
