167b416df8cacc0133ac1d0daf846bf9e44bb836
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-23 16:31:18 +0600

    ts asynchronous

--- modules/src/Module 2/asynchronous.ts
+++ modules/src/Module 2/asynchronous.ts
@@ -1,36 +1,36 @@
-const makePromise = (): Promise<string> => {
-    return new Promise<string>((resolve, reject) => {
-        const data: string = 'Data is fetch';
-        if (data) {
-            resolve(data);
-        }
-        else {
-            reject('fetch fail')
-        }
-    })
-}
+// const makePromise = (): Promise<string> => {
+//     return new Promise<string>((resolve, reject) => {
+//         const data: string = 'Data is fetch';
+//         if (data) {
+//             resolve(data);
+//         }
+//         else {
+//             reject('fetch fail')
+//         }
+//     })
+// }
 
 
-const getPromise = async (): Promise<string> => {
-    const data = await makePromise();
-    return data;
-}
+// const getPromise = async (): Promise<string> => {
+//     const data = await makePromise();
+//     return data;
+// }
 
-interface ITodo {
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
-}
+// interface ITodo {
+//     userId: number,
+//     id: number,
+//     title: string,
+//     completed: boolean
+// }
 
-// Load PlaceHolder data using async function 
-const getToDo = async (): Promise<ITodo> => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    return await response.json();
-}
+// // Load PlaceHolder data using async function 
+// const getToDo = async (): Promise<ITodo> => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     return await response.json();
+// }
 
-const getResult = async () => {
-    const result = await getToDo();
-    console.log(result)
-}
-getResult()
+// const getResult = async () => {
+//     const result = await getToDo();
+//     console.log(result)
+// }
+// getResult()
