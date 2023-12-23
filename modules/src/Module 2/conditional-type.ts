cd06b00b5ecd0efdb1b537400a5147e54511075e
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-23 16:42:59 +0600

    ts conditional type

--- modules/src/Module 2/conditional-type.ts
+++ modules/src/Module 2/conditional-type.ts
@@ -7,4 +7,4 @@ type a3 = null;
 type a2 = a1 extends string ? string : null;
 
 //  nested conditional type
-type a4 = a1 extends number ? number : a3 extends undefined ? string : null
+type a4 = a1 extends number ? number : a3 extends undefined ? string : null 
