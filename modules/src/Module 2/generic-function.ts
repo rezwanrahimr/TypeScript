300f54265f127aa0842bc747a899bf6c6212449e
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-23 15:38:20 +0600

    ts asynchronous

---
+++ modules/src/Module 2/generic-function.ts
@@ -0,0 +1,13 @@
+
+//TypeScript Generic Function
+
+
+const createArray = (param: string): string[] => { // normal function
+    return [param];
+}
+
+const createArray2 = <T>(param: T): T[] => {
+    return [param];
+}
+
+console.log(createArray2<string>('2'));
--- modules/src/Module 2/generic-interface.ts
+++ modules/src/Module 2/generic-interface.ts
@@ -1,36 +1,36 @@
-// Generic Interface
+// // Generic Interface
 
-interface CrushInterface<T, U = null> {
-    name: string,
-    husband: T,
-    marred?: U
-}
+// interface CrushInterface<T, U = null> {
+//     name: string,
+//     husband: T,
+//     marred?: U
+// }
 
 
-const crush1: CrushInterface<boolean, { isMarred: boolean }> = {
-    name: 'Kate Winslet',
-    husband: true,
-    marred: {
-        isMarred: true
-    }
-}
+// const crush1: CrushInterface<boolean, { isMarred: boolean }> = {
+//     name: 'Kate Winslet',
+//     husband: true,
+//     marred: {
+//         isMarred: true
+//     }
+// }
 
-const crush2: CrushInterface<string, boolean> = {
-    name: 'kate winslet',
-    husband: 'sofik',
-    marred: true
+// const crush2: CrushInterface<string, boolean> = {
+//     name: 'kate winslet',
+//     husband: 'sofik',
+//     marred: true
 
-}
+// }
 
-interface HusbandInterface {
-    name: string,
-    age: number
-}
+// interface HusbandInterface {
+//     name: string,
+//     age: number
+// }
 
-const crush3: CrushInterface<HusbandInterface> = {
-    name: 'kate winslet',
-    husband: {
-        name: 'sofik',
-        age: 30
-    }
-}
+// const crush3: CrushInterface<HusbandInterface> = {
+//     name: 'kate winslet',
+//     husband: {
+//         name: 'sofik',
+//         age: 30
+//     }
+// }
