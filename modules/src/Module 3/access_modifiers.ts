8c0d3ed8f4066ec86b78b6fb03e7aaf89a0a2d58
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-24 17:05:04 +0600

    ts access modifiers

---
+++ modules/src/Module 3/access_modifiers.ts
@@ -0,0 +1,14 @@
+class BankAccount {
+    id: number;
+    name: string;
+    private balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+        this.id = id
+        this.name = name
+        this.balance = balance
+    }
+}
+
+const newAccount = new BankAccount(1, 'Rezwan Rahim', 20);
+console.log(newAccount);
