08ad1c8f11a21afc73e949b9c29a6ea4571cb384
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-25 23:15:00 +0600

    problem 7 solved

--- modules/src/Module 3.5/problem6.ts
+++ modules/src/Module 3.5/problem6.ts
@@ -1,7 +1,7 @@
 /* Create an interface called Person that includes properties for name (string), age (number), and email (string). Then create an array of Person objects and write a function that takes the array and a string email as parameters, and returns the Person object that matches the email or null if no match is found.
  */
 
-interface Person {
+/* interface Person {
   name: string;
   age: number;
   email: string;
@@ -18,3 +18,4 @@ let persons: Person[] = [
 ];
 
 console.log(findPerson(persons, "rezwanrahim99@gmail.com"));
+ */
---
+++ modules/src/Module 3.5/problem7.ts
@@ -0,0 +1,16 @@
+/* Create a TypeScript program that declares an array of numbers. Use the spread  operator to pass the elements of the array as arguments to a function that finds the minimum and maximum values of the array. Use destructuring to assign the minimum and maximum values to separate variables, and log them to the console. */
+
+/* function findMinimumAndMaximum(...numbers: number[]) {
+  let minimum = numbers[0];
+  let maximum = numbers[0];
+  numbers.forEach((item) => {
+    if (minimum > item) {
+      minimum = item;
+    } else if (maximum < item) {
+      maximum = item;
+    }
+  });
+  return `Maximum Number is: ${maximum} & Minimum Number is: ${minimum}`;
+}
+
+console.log(findMinimumAndMaximum(1, 2, 3, 4, 5)); */
