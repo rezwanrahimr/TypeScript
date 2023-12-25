9fb860d98e0b7672bd9bb42809190ed127499bb5
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-25 23:24:27 +0600

    problem 8 solved

---
+++ modules/src/Module 3.5/problem8.ts
@@ -0,0 +1,13 @@
+/* Create a TypeScript program that declares a function that takes a string parameter with a literal type of "red", "green", or "blue", and an optional boolean parameter. If the boolean parameter is true, log the string parameter in uppercase. If the boolean parameter is false or not provided, log the string parameter in lowercase. */
+
+/* function textGenerator(text: string, type?: boolean) {
+  if (type && type === true) {
+    return text.toUpperCase();
+  } else if (type && type === false) {
+    return text.toLocaleLowerCase();
+  } else {
+    return text.toLocaleLowerCase();
+  }
+}
+
+console.log(textGenerator("red", true)); */
