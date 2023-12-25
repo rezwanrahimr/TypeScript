9dfdd33e882e4b4d3a4bac98c4def373c226c85f
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-25 21:03:59 +0600

    problem 4 solved

--- modules/src/Module 3.5/problem3.ts
+++ modules/src/Module 3.5/problem3.ts
@@ -1,26 +1,26 @@
-/* You have an interface for Product, containing the product's id, name, price, and category. You want to filter an array of Products based on a specific criterion and value.
+// /* You have an interface for Product, containing the product's id, name, price, and category. You want to filter an array of Products based on a specific criterion and value.
 
-Write a TypeScript generic function that takes this array, a criterion , and returns a new array containing only the products that match the given criterion and value. Use a generic type parameter in the function signature to ensure type safety. */
+// Write a TypeScript generic function that takes this array, a criterion , and returns a new array containing only the products that match the given criterion and value. Use a generic type parameter in the function signature to ensure type safety. */
 
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-  category: string;
-}
+// interface Product {
+//   id: number;
+//   name: string;
+//   price: number;
+//   category: string;
+// }
 
-function filterProduct<T extends Product>(
-  products: T[],
-  type: keyof Product,
-  value: T[keyof Product]
-): T[] {
-  const getProduct = products.filter((product: T) => product[type] === value);
-  return getProduct;
-}
-const productData = [
-  { id: 1, name: "Product one", price: 23, category: "new" },
-  { id: 2, name: "Product two", price: 30, category: "old" },
-];
-const getProducts = filterProduct<Product>(productData, "category", "old");
+// function filterProduct<T extends Product>(
+//   products: T[],
+//   type: keyof Product,
+//   value: T[keyof Product]
+// ): T[] {
+//   const getProduct = products.filter((product: T) => product[type] === value);
+//   return getProduct;
+// }
+// const productData = [
+//   { id: 1, name: "Product one", price: 23, category: "new" },
+//   { id: 2, name: "Product two", price: 30, category: "old" },
+// ];
+// const getProducts = filterProduct<Product>(productData, "category", "old");
 
-console.log(getProducts);
+// console.log(getProducts);
---
+++ modules/src/Module 3.5/problem4.ts
@@ -0,0 +1,24 @@
+/* Suppose you have an array of tuples, where each tuple represents a product and contains the product name, price, and quantity. Write a TypeScript function that calculates the total cost of all the products in the array, using a generic type for the tuple and a type alias for the array.
+ */
+
+type Product = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+function CalculateProduct<T extends Product[]>(product: T): number {
+  let total = 0;
+  product.forEach((item) => {
+    total = total + item.price * item.quantity;
+  });
+  return total;
+}
+
+const products: Product[] = [
+  { productName: "Shirt", price: 20, quantity: 3 },
+  { productName: "Jeans", price: 30, quantity: 2 },
+];
+
+const totalAmount = CalculateProduct(products);
+console.log(`Total Amount of Products ${totalAmount}`);
