294bb50c975c2caa079e62655f996aa23ae85d51
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-27 10:08:30 +0600

    TypeScript Array & Tuples

---
+++ modules/src/Session/Array_Tuples.ts
@@ -0,0 +1,8 @@
+// TypeScript Array
+/* const hero: string[] = ['Programming Hero', 'Web Development'];
+const num: number[] = [2, 3, 4, 5];
+const heroInfo: (string | number | boolean)[] = ['Rezwan Rahim', 23, true]; */
+
+
+// TypeScript Tuple
+/* const heroTuple: [number, string, number, boolean] = [1, 'Rezwan Rahim', 23, true]; */
--- modules/src/Session/index.ts
+++ modules/src/Session/index.ts
@@ -8,6 +8,6 @@ Undefined
 */
 
 // Declear variable with type in TypeScript
-const str: string = "Rezwan Rahim";
+/* const str: string = "Rezwan Rahim";
 const num: number = 1678392594;
-const isNumber: boolean = true;
+const isNumber: boolean = true; */
