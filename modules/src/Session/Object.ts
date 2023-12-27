aa1fb009b05c4f435888967678e331ab2f7ac000
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-27 10:38:58 +0600

    TypeScript Object

---
+++ modules/src/Session/Object.ts
@@ -0,0 +1,13 @@
+/* const userInfo: {
+   readonly name: string;
+    age?: number;
+    profession: string;
+    isMarried: boolean
+} = {
+    name: 'Rezwan Rahim',
+    age: 23,
+    profession: 'Jr Developer',
+    isMarried: false
+}
+
+console.log(userInfo) */
