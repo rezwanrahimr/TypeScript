ce00ac62cbdab0050d7dcaeef1c34d704bbd8e73
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-27 10:56:41 +0600

    TypeScript Function

---
+++ modules/src/Session/Function.ts
@@ -0,0 +1,10 @@
+// TypeScript Normal Function
+/* function normalFunc(num1: number, num2: number): number {
+    return num1 + num2;
+} */
+
+
+// TypeScript Arrow Function
+/* const arrowFunc = (num1: number, num2: number): number => {
+    return num1 + num2;
+} */
