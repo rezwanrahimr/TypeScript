c852e0843985bb3d65ffd6adc7f874bb6c45b612
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-27 11:30:30 +0600

    TypeScript Spread Operator & Rest Parameter

---
+++ modules/src/Session/Spread_Rest.ts
@@ -0,0 +1,16 @@
+// TypeScript Spread Operator
+/* const score = [1, 2, 3, 4, 5];
+const score2 = [6, 7, 8, 9];
+score.push(...score2); */
+
+
+// TypeScript Rest Parameter
+/* const totalNum = (...num: number[]): number => {
+    let total = 0;
+    num.forEach((num) => {
+        total = total + num;
+    })
+    return total;
+}
+
+console.log(totalNum(2,3,4,5,6,7)); */
