c30fa169bdd65647a239a773f1be6d0ea10815e6
rezwanrahimr <rezwanrahim69@gmail.com>
2023-12-27 09:47:06 +0600

    revision typescript concept

---
+++ modules/src/Session/index.ts
@@ -0,0 +1,13 @@
+// Primitive Types
+/* 
+String
+Number
+Boolean
+Null
+Undefined
+*/
+
+// Declear variable with type in TypeScript
+const str: string = "Rezwan Rahim";
+const num: number = 1678392594;
+const isNumber: boolean = true;
