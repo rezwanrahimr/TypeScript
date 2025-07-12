e37563926fc3a7ac6dd8e8cac91bcfba8e34d382
DESKTOP-F25VPLM\Rezwan Rahim <rezwanrahim69@gmail.com>
2025-07-12 21:42:33 +0600

    recape ts types

---
+++ modules/src/Recap/1.4.ts
@@ -0,0 +1,24 @@
+// Ts Basic Data Types
+
+// string
+let myName:string = 'rezwan rahim';
+
+// number
+let roll:number = 31;
+
+// boolean
+let isActive:boolean = true;
+
+// null
+let isExist:null = null;
+
+// undefiend
+let nickName:undefined = undefined;
+
+// array
+let employees:string[] = ['rezwan','rahim'];
+
+let employeesCode:number[] = [1,2];
+
+// tuple
+let employeeDetails:[string,number] = ['Rezwan Rahim',22000];
---
+++ modules/src/Recap/1.5.ts
@@ -0,0 +1,11 @@
+const person:{
+    readonly companyName?:'Programming Hero',
+    firstName:string,
+    middleName?:string,
+    lastName:string,
+    isMarried:boolean
+} = {
+    firstName:'Rezwan',
+    lastName:'Rupak',
+    isMarried:false
+}
