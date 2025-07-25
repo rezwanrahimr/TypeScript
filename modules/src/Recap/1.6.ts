021224dc03bec2319449eab6519753907a50440b
Rezwan Rahim Rupak <rezwanrahim.rupak@gmail.com>
2025-07-25 09:42:22 +0600

    typescript function

--- modules/src/Recap/1.6.ts
+++ modules/src/Recap/1.6.ts
@@ -1,3 +1,25 @@
+// learning functions in typescript
+// normal function
+// arrow function
+
+
+function addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+const addNumbersArrow = (num1: number, num2: number): number => num1 + num2;
+
+const user = {
+    name: 'Rezwan',
+    balance: 0,
+    addBalance(balance: number): number {
+        return this.balance + balance;
+    }
+}
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+const result = numbers.map((element: number): number => element * element);
+
 function generateHello(message: string): string {
     return `Hello ${message}`;
 }
